d8a8d9d12410eb693f21f6143eb14a9ceff8cda7
Ogundare Olusesi Oluwafemi <ogundarearold@gmail.com>
2023-11-16 15:52:32 +0100

    used the latest flutter update

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,6 @@ class ShoppingList extends StatelessWidget {
       title: 'Flutter Groceries',
       routerConfig: _router,
       theme: ThemeData.dark().copyWith(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
